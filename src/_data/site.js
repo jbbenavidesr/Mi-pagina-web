5f3953f58cbb09a8e77ae95cc48d1247af46aec5
Juan Bernardo Benavides <jbbenavidesr@unal.edu.co>
2021-06-24 18:30:21 -0500

    Set up some config files

---
+++ src/_data/site.js
@@ -0,0 +1,14 @@
+module.exports = {
+    buildTime: new Date(),
+    baseUrl: 'https://www.jbbenavidesr.com',
+    name: 'jbbenavidesr',
+    twitter: '@jbbenavidesr',
+    es: {
+        metaTitle: 'Juan B Benavides R',
+        metaDescription: 'El blog de un loco más.',
+    },
+    en: {
+        metaTitle: 'Juan B Benavides R',
+        metaDescription: "Another crazy man's blog",
+    },
+};
